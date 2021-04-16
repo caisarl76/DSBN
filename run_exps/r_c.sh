0452ca3b2db18ea194b1e6004a15b4d4a1c1de38
donggun <unlimitlife@naver.com>
2021-04-16 02:25:31 -0400

    add shell codes

--- run_exps/r_c.sh
+++ run_exps/r_c.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=7 python trainval_multi.py --model-name resnet50dsbn --exp-setting office-home --sm-loss --adv-loss \
+ 	--source-datasets RealWorld --target-datasets Clipart \
+	--batch-size 40 --print-console --save-dir /mnt/dsbn_result/original_dsbn/r_c/stage1
+
+
+
+CUDA_VISIBLE_DEVICES=7 python finetune_multi.py --model-name resnet50dsbn --exp-setting office-home \
 	--source-datasets RealWorld --target-datasets Clipart \
-	--batch-size 40 --print-console --save-dir /mnt/dsbn_result/original_dsbn/r_c/stage1 
+       	--pseudo-target-loss default_ensemble --no-lambda \
+	--teacher-model-path /mnt/dsbn_result/original_dsbn/r_c/stage1/best_resnet50dsbn+None+i0_RealWorld2Clipart.pth \
+	--learning-rate 5e-5 --batch-size 40 --save-dir /mnt/dsbn_result/original_dsbn/r_c/stage2 --print-console
+
 
 
--- run_exps/r_p.sh
+++ run_exps/r_p.sh
@@ -4,4 +4,9 @@ CUDA_VISIBLE_DEVICES=5 python trainval_multi.py --model-name resnet50dsbn --exp-
 	--source-datasets RealWorld --target-datasets Product \
 	--batch-size 40 --print-console --save-dir /mnt/dsbn_result/original_dsbn/r_p/stage1 
 
+CUDA_VISIBLE_DEVICES=5 python finetune_multi.py --model-name resnet50dsbn --exp-setting office-home \
+	--source-datasets RealWorld --target-datasets Product \
+	--pseudo-target-loss default_ensemble --no-lambda \
+	--teacher-model-path /mnt/dsbn_result/original_dsbn/r_p/stage1/best_resnet50dsbn+None+i0_RealWorld2Product.pth \
+	--learning-rate 5e-5 --batch-size 40 --save-dir /mnt/dsbn_result/original_dsbn/r_p/stage2 --print-console
 
